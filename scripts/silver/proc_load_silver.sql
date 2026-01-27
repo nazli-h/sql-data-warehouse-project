95492b8546dbeaceba036c415c8b96d3fb4939fa
Nazli Ertekin <138868236+nazli-h@users.noreply.github.com>
2026-01-27 21:57:14 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -34,8 +34,46 @@ BEGIN
 -- Loading silver.crm_cust_info
      SET @start_time = GETDATE();
 PRINT '>> Truncating Table: silver.crm_cust_info';
+TRUNCATE TABLE silver.crm_cust_info;
+PRINT '>> Inserting Data Into: silver.crm_cust_info';
 INSERT INTO silver.crm_cust_info(
   cst_id,
+  cst_key,
+  cst_firstname,
+  cst_lastname,
+  cst_marital_status,
+  cst_gndr,
+  cst_create_date
+  )
+ SELECT 
+   cst_id,
+   cst_key,
+   TRIM(cst_firstname) AS cst_firstname,
+   TRIM(cst_lastname) AS cst_lastname,
+   CASE
+     WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+     WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+     ELSE 'n/a'
+   END AS cst_marital_status,
+   CASE
+     WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+     WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+     ELSE 'n/a'
+   END AS cst_gndr,
+   cst_create_date
+FROM (
+  SELECT 
+     *,
+     ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+   FROM bronze.crm_cust_info
+   WHERE cst_id IS NOT NULL
+ ) t
+ WHERe flag_lat = 1;
+SET @end_time = GETDATE();
+    PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) +' seconds';
+    PRINT '>> -------------------------';
+
+     
 
 
 
